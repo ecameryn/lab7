6cab1b24885bd2693166b3d7b31eb750be5c9f67
Cameryn Holstick <cholstick0104@gmail.com>
2016-10-08 20:43:39 -0400

    mysqli version

--- wishlist.php
+++ wishlist.php
@@ -1,37 +1,32 @@
 <!DOCTYPE html>
-<?php
-    $dsn = 'mysql:host=localhost;dbname=Wishlist_DB';
-    $username = 'phpuser';
-    $password = 'pa55word';
-
-    try {
-        $db = new PDO($dsn, $username, $password);
-    } catch (PDOException $e) {
-        $error_message = $e->getMessage();
-		echo 'Database error!';
-        exit();
-    }
-	
-function get_user($user)
-{
-	global $db;
-	$query = "SELECT * FROM wishers
-			 WHERE wishers.name = '$user'";	 
-	$result = $db->query($query);
-	return $result;
-}
-	$user = get_user($_GET['user']);
-?>
 
 <html>
 	<head>
 		<title></title>
 	</head>
 	
-	
 	<body>Wish List of <?php echo $_GET['user'] ; ?>
 		<?php 
-		
+			$con = mysqli_connect("localhost", "phpuser", "pa55word");
+			if (!$con) 
+			{
+				exit('Connect Error (' . mysqli_connect_errno() . ') '
+				   . mysqli_connect_error());
+			}
+			//set the default client character set 
+			mysqli_set_charset($con, 'utf-8');
+			
+			mysqli_select_db($con, "wishlist");
+			$user = mysqli_real_escape_string($con, htmlentities($_GET["user"]));
+			$wisher = mysqli_query($con, "SELECT id FROM wishers WHERE name='" . $user . "'");
+
+			if (mysqli_num_rows($wisher) < 1) 
+			{
+				exit("The person " . htmlentities($_GET["user"]) . " is not found. Please check the spelling and try again");
+			}
+			$row = mysqli_fetch_row($wisher);
+			$wisherID = $row[0];
+			mysqli_free_result($wisher);	
 		?>
 	</body>
 </html>
